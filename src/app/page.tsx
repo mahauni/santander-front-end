d8b88e95a052d5a37cea769559e428202e6d6e0d
Lucas Raoni <lucasraoniha@gmail.com>
2025-08-04 00:25:41 -0300

    WIP: getting fields value on login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,16 +3,25 @@ import { TextField } from "@mui/material";
 import styles from "./page.module.css";
 import Button from '@mui/material/Button';
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 export default function Home() {
     const router = useRouter()
+    const [email, setEmail] = useState<string | undefined>()
+    const [password, setPassword] = useState<string | undefined>()
+
+    function onLogin() {
+        console.log(email, password)
+
+        router.push('/analizer')
+    }
 
     return (
         <div className={styles.page}>
             <main className={styles.main}>
-                <TextField label="Email" required={true} style={{ color: 'white' }} variant="outlined" />
-                <TextField label="Passoword" required={true} style={{ color: 'white' }} variant="outlined" />
-                <Button onClick={() => router.push('/analizer')}>Login</Button>
+                <TextField label="Email" required={true} onChange={(e) => setEmail(e.target.value)} style={{ color: 'white' }} variant="outlined" />
+                <TextField label="Password" onChange={(e) => setPassword(e.target.value)} required={true} style={{ color: 'white' }} variant="outlined" />
+                <Button onClick={onLogin}>Login</Button>
             </main>
         </div>
     );
